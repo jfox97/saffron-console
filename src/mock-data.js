28eee6dbdb966411450adaa28f736e6cf67fcd68
Jason Fox <jfox97@gmail.com>
2020-03-15 11:46:36 -0600

    Add mock data

---
+++ src/mock-data.js
@@ -0,0 +1,702 @@
+let mock = [
+  {
+    id: 1,
+    date: '2019-02-16',
+    description: 'Skilith',
+    category: 'Shoes',
+    amount: '$114.61'
+  },
+  {
+    id: 2,
+    date: '2019-06-13',
+    description: 'Zooxo',
+    category: 'Beauty',
+    amount: '$78.55'
+  },
+  {
+    id: 3,
+    date: '2019-04-28',
+    description: 'Blogtags',
+    category: 'Outdoors',
+    amount: '$54.66'
+  },
+  {
+    id: 4,
+    date: '2019-02-08',
+    description: 'Skajo',
+    category: 'Baby',
+    amount: '$95.35'
+  },
+  {
+    id: 5,
+    date: '2020-02-04',
+    description: 'Lajo',
+    category: 'Kids',
+    amount: '$60.57'
+  },
+  {
+    id: 6,
+    date: '2019-05-27',
+    description: 'Miboo',
+    category: 'Outdoors',
+    amount: '$259.33'
+  },
+  {
+    id: 7,
+    date: '2019-06-16',
+    description: 'Avamba',
+    category: 'Beauty',
+    amount: '$88.71'
+  },
+  {
+    id: 8,
+    date: '2019-06-28',
+    description: 'Tagpad',
+    category: 'Games',
+    amount: '$229.21'
+  },
+  {
+    id: 9,
+    date: '2019-07-15',
+    description: 'Yata',
+    category: 'Toys',
+    amount: '$235.14'
+  },
+  {
+    id: 10,
+    date: '2019-02-28',
+    description: 'Podcat',
+    category: 'Kids',
+    amount: '$242.68'
+  },
+  {
+    id: 11,
+    date: '2020-01-18',
+    description: 'Feedspan',
+    category: 'Shoes',
+    amount: '$210.56'
+  },
+  {
+    id: 12,
+    date: '2019-10-25',
+    description: 'Gabtype',
+    category: 'Electronics',
+    amount: '$208.75'
+  },
+  {
+    id: 13,
+    date: '2019-02-07',
+    description: 'Gigazoom',
+    category: 'Clothing',
+    amount: '$282.39'
+  },
+  {
+    id: 14,
+    date: '2019-09-26',
+    description: 'Youbridge',
+    category: 'Shoes',
+    amount: '$69.41'
+  },
+  {
+    id: 15,
+    date: '2019-04-17',
+    description: 'Yakidoo',
+    category: 'Computers',
+    amount: '$143.44'
+  },
+  {
+    id: 16,
+    date: '2019-07-05',
+    description: 'Tagopia',
+    category: 'Music',
+    amount: '$26.84'
+  },
+  {
+    id: 17,
+    date: '2020-02-10',
+    description: 'Livetube',
+    category: 'Computers',
+    amount: '$269.83'
+  },
+  {
+    id: 18,
+    date: '2020-02-24',
+    description: 'Twitterbeat',
+    category: 'Outdoors',
+    amount: '$219.27'
+  },
+  {
+    id: 19,
+    date: '2020-01-01',
+    description: 'Jabbertype',
+    category: 'Music',
+    amount: '$226.00'
+  },
+  {
+    id: 20,
+    date: '2020-03-11',
+    description: 'Jetpulse',
+    category: 'Industrial',
+    amount: '$93.95'
+  },
+  {
+    id: 21,
+    date: '2019-05-31',
+    description: 'Zoonoodle',
+    category: 'Home',
+    amount: '$203.79'
+  },
+  {
+    id: 22,
+    date: '2020-02-11',
+    description: 'Trunyx',
+    category: 'Automotive',
+    amount: '$74.17'
+  },
+  {
+    id: 23,
+    date: '2020-01-06',
+    description: 'Thoughtbeat',
+    category: 'Grocery',
+    amount: '$204.07'
+  },
+  {
+    id: 24,
+    date: '2020-02-25',
+    description: 'Topdrive',
+    category: 'Kids',
+    amount: '$42.26'
+  },
+  {
+    id: 25,
+    date: '2019-08-13',
+    description: 'Fivespan',
+    category: 'Garden',
+    amount: '$171.05'
+  },
+  {
+    id: 26,
+    date: '2019-11-22',
+    description: 'Eimbee',
+    category: 'Beauty',
+    amount: '$7.02'
+  },
+  {
+    id: 27,
+    date: '2019-08-30',
+    description: 'Feedmix',
+    category: 'Tools',
+    amount: '$151.63'
+  },
+  {
+    id: 28,
+    date: '2019-05-16',
+    description: 'Oyoloo',
+    category: 'Home',
+    amount: '$258.67'
+  },
+  {
+    id: 29,
+    date: '2020-03-13',
+    description: 'Abatz',
+    category: 'Home',
+    amount: '$172.79'
+  },
+  {
+    id: 30,
+    date: '2019-05-15',
+    description: 'Yakidoo',
+    category: 'Industrial',
+    amount: '$255.22'
+  },
+  {
+    id: 31,
+    date: '2019-02-10',
+    description: 'Tagopia',
+    category: 'Baby',
+    amount: '$145.56'
+  },
+  {
+    id: 32,
+    date: '2019-10-01',
+    description: 'Topicstorm',
+    category: 'Sports',
+    amount: '$133.08'
+  },
+  {
+    id: 33,
+    date: '2019-06-29',
+    description: 'Mydo',
+    category: 'Health',
+    amount: '$67.88'
+  },
+  {
+    id: 34,
+    date: '2019-05-29',
+    description: 'Photobug',
+    category: 'Sports',
+    amount: '$78.50'
+  },
+  {
+    id: 35,
+    date: '2020-01-31',
+    description: 'Tagcat',
+    category: 'Jewelery',
+    amount: '$61.25'
+  },
+  {
+    id: 36,
+    date: '2019-12-25',
+    description: 'Jabbercube',
+    category: 'Kids',
+    amount: '$34.10'
+  },
+  {
+    id: 37,
+    date: '2020-02-10',
+    description: 'Dabtype',
+    category: 'Movies',
+    amount: '$273.52'
+  },
+  {
+    id: 38,
+    date: '2020-02-25',
+    description: 'Oloo',
+    category: 'Automotive',
+    amount: '$294.72'
+  },
+  {
+    id: 39,
+    date: '2019-03-10',
+    description: 'Topicshots',
+    category: 'Garden',
+    amount: '$249.72'
+  },
+  {
+    id: 40,
+    date: '2019-11-15',
+    description: 'Topicware',
+    category: 'Toys',
+    amount: '$150.27'
+  },
+  {
+    id: 41,
+    date: '2019-03-06',
+    description: 'Yotz',
+    category: 'Grocery',
+    amount: '$109.91'
+  },
+  {
+    id: 42,
+    date: '2019-08-24',
+    description: 'Kazio',
+    category: 'Clothing',
+    amount: '$130.38'
+  },
+  {
+    id: 43,
+    date: '2020-03-07',
+    description: 'Gevee',
+    category: 'Sports',
+    amount: '$140.22'
+  },
+  {
+    id: 44,
+    date: '2020-01-20',
+    description: 'Skibox',
+    category: 'Garden',
+    amount: '$143.58'
+  },
+  {
+    id: 45,
+    date: '2019-02-07',
+    description: 'Viva',
+    category: 'Outdoors',
+    amount: '$142.16'
+  },
+  {
+    id: 46,
+    date: '2019-03-16',
+    description: 'Layo',
+    category: 'Jewelery',
+    amount: '$160.76'
+  },
+  {
+    id: 47,
+    date: '2020-02-06',
+    description: 'Flipstorm',
+    category: 'Garden',
+    amount: '$228.20'
+  },
+  {
+    id: 48,
+    date: '2019-12-12',
+    description: 'Pixope',
+    category: 'Outdoors',
+    amount: '$133.26'
+  },
+  {
+    id: 49,
+    date: '2019-07-20',
+    description: 'Cogilith',
+    category: 'Garden',
+    amount: '$198.98'
+  },
+  {
+    id: 50,
+    date: '2019-09-18',
+    description: 'Flipstorm',
+    category: 'Automotive',
+    amount: '$241.31'
+  },
+  {
+    id: 51,
+    date: '2019-07-19',
+    description: 'Jabbercube',
+    category: 'Movies',
+    amount: '$27.93'
+  },
+  {
+    id: 52,
+    date: '2019-12-02',
+    description: 'Podcat',
+    category: 'Shoes',
+    amount: '$164.75'
+  },
+  {
+    id: 53,
+    date: '2019-02-06',
+    description: 'Kwinu',
+    category: 'Baby',
+    amount: '$217.58'
+  },
+  {
+    id: 54,
+    date: '2019-04-30',
+    description: 'Katz',
+    category: 'Music',
+    amount: '$53.99'
+  },
+  {
+    id: 55,
+    date: '2019-08-19',
+    description: 'Realbridge',
+    category: 'Outdoors',
+    amount: '$30.00'
+  },
+  {
+    id: 56,
+    date: '2019-11-02',
+    description: 'Linktype',
+    category: 'Movies',
+    amount: '$248.06'
+  },
+  {
+    id: 57,
+    date: '2019-05-02',
+    description: 'Fivechat',
+    category: 'Electronics',
+    amount: '$188.45'
+  },
+  {
+    id: 58,
+    date: '2019-03-28',
+    description: 'Dabfeed',
+    category: 'Automotive',
+    amount: '$220.21'
+  },
+  {
+    id: 59,
+    date: '2019-11-15',
+    description: 'Jayo',
+    category: 'Outdoors',
+    amount: '$58.47'
+  },
+  {
+    id: 60,
+    date: '2019-02-20',
+    description: 'Gigaclub',
+    category: 'Automotive',
+    amount: '$230.78'
+  },
+  {
+    id: 61,
+    date: '2019-12-18',
+    description: 'Realmix',
+    category: 'Kids',
+    amount: '$74.84'
+  },
+  {
+    id: 62,
+    date: '2019-11-06',
+    description: 'Gabtune',
+    category: 'Toys',
+    amount: '$299.29'
+  },
+  {
+    id: 63,
+    date: '2019-03-06',
+    description: 'Mycat',
+    category: 'Electronics',
+    amount: '$109.73'
+  },
+  {
+    id: 64,
+    date: '2019-12-16',
+    description: 'Roombo',
+    category: 'Grocery',
+    amount: '$146.10'
+  },
+  {
+    id: 65,
+    date: '2019-04-07',
+    description: 'Jabberbean',
+    category: 'Health',
+    amount: '$128.69'
+  },
+  {
+    id: 66,
+    date: '2019-04-30',
+    description: 'Devshare',
+    category: 'Shoes',
+    amount: '$176.09'
+  },
+  {
+    id: 67,
+    date: '2019-07-05',
+    description: 'Pixoboo',
+    category: 'Shoes',
+    amount: '$68.10'
+  },
+  {
+    id: 68,
+    date: '2019-02-22',
+    description: 'Jaxnation',
+    category: 'Baby',
+    amount: '$6.99'
+  },
+  {
+    id: 69,
+    date: '2019-08-30',
+    description: 'Dynazzy',
+    category: 'Shoes',
+    amount: '$179.00'
+  },
+  {
+    id: 70,
+    date: '2019-11-19',
+    description: 'Wordware',
+    category: 'Shoes',
+    amount: '$143.76'
+  },
+  {
+    id: 71,
+    date: '2019-04-20',
+    description: 'Cogilith',
+    category: 'Jewelery',
+    amount: '$27.78'
+  },
+  {
+    id: 72,
+    date: '2019-06-06',
+    description: 'Skyble',
+    category: 'Movies',
+    amount: '$196.56'
+  },
+  {
+    id: 73,
+    date: '2019-07-11',
+    description: 'Voomm',
+    category: 'Home',
+    amount: '$51.27'
+  },
+  {
+    id: 74,
+    date: '2019-09-25',
+    description: 'Kazu',
+    category: 'Clothing',
+    amount: '$249.47'
+  },
+  {
+    id: 75,
+    date: '2019-04-09',
+    description: 'Janyx',
+    category: 'Health',
+    amount: '$78.77'
+  },
+  {
+    id: 76,
+    date: '2019-03-18',
+    description: 'Flashdog',
+    category: 'Jewelery',
+    amount: '$194.88'
+  },
+  {
+    id: 77,
+    date: '2019-12-27',
+    description: 'Topicshots',
+    category: 'Electronics',
+    amount: '$45.07'
+  },
+  {
+    id: 78,
+    date: '2020-01-02',
+    description: 'Zoomdog',
+    category: 'Shoes',
+    amount: '$110.42'
+  },
+  {
+    id: 79,
+    date: '2019-10-03',
+    description: 'Twinte',
+    category: 'Health',
+    amount: '$98.44'
+  },
+  {
+    id: 80,
+    date: '2019-06-19',
+    description: 'Jaxspan',
+    category: 'Electronics',
+    amount: '$70.88'
+  },
+  {
+    id: 81,
+    date: '2019-04-28',
+    description: 'Dazzlesphere',
+    category: 'Shoes',
+    amount: '$73.55'
+  },
+  {
+    id: 82,
+    date: '2019-05-15',
+    description: 'Eare',
+    category: 'Industrial',
+    amount: '$146.61'
+  },
+  {
+    id: 83,
+    date: '2019-11-19',
+    description: 'Kare',
+    category: 'Games',
+    amount: '$107.32'
+  },
+  {
+    id: 84,
+    date: '2019-11-06',
+    description: 'Voomm',
+    category: 'Baby',
+    amount: '$39.21'
+  },
+  {
+    id: 85,
+    date: '2019-06-20',
+    description: 'Plambee',
+    category: 'Music',
+    amount: '$169.08'
+  },
+  {
+    id: 86,
+    date: '2019-09-13',
+    description: 'Tagchat',
+    category: 'Beauty',
+    amount: '$63.15'
+  },
+  {
+    id: 87,
+    date: '2019-10-25',
+    description: 'Jabberstorm',
+    category: 'Music',
+    amount: '$203.30'
+  },
+  {
+    id: 88,
+    date: '2019-12-29',
+    description: 'Topicblab',
+    category: 'Outdoors',
+    amount: '$252.57'
+  },
+  {
+    id: 89,
+    date: '2019-11-20',
+    description: 'Gigazoom',
+    category: 'Books',
+    amount: '$172.19'
+  },
+  {
+    id: 90,
+    date: '2019-03-18',
+    description: 'Zoomcast',
+    category: 'Books',
+    amount: '$57.47'
+  },
+  {
+    id: 91,
+    date: '2019-12-22',
+    description: 'Yambee',
+    category: 'Home',
+    amount: '$21.34'
+  },
+  {
+    id: 92,
+    date: '2019-04-07',
+    description: 'Zoombox',
+    category: 'Music',
+    amount: '$181.60'
+  },
+  {
+    id: 93,
+    date: '2020-01-27',
+    description: 'Bubbletube',
+    category: 'Movies',
+    amount: '$101.22'
+  },
+  {
+    id: 94,
+    date: '2019-11-02',
+    description: 'Pixope',
+    category: 'Music',
+    amount: '$151.88'
+  },
+  {
+    id: 95,
+    date: '2020-01-22',
+    description: 'Eire',
+    category: 'Garden',
+    amount: '$124.91'
+  },
+  {
+    id: 96,
+    date: '2020-01-06',
+    description: 'Eimbee',
+    category: 'Home',
+    amount: '$98.42'
+  },
+  {
+    id: 97,
+    date: '2019-10-25',
+    description: 'Dabvine',
+    category: 'Garden',
+    amount: '$117.26'
+  },
+  {
+    id: 98,
+    date: '2019-07-02',
+    description: 'Yodel',
+    category: 'Kids',
+    amount: '$21.72'
+  },
+  {
+    id: 99,
+    date: '2019-10-27',
+    description: 'Leenti',
+    category: 'Outdoors',
+    amount: '$299.41'
+  },
+  {
+    id: 100,
+    date: '2019-12-04',
+    description: 'Demimbu',
+    category: 'Home',
+    amount: '$203.00'
+  }
+]
