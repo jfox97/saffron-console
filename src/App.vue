c2031d3699f2697bb85be0903db056e5b9412b0b
Jason Fox <jfox97@gmail.com>
2020-03-15 12:14:10 -0600

    Add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div id="brand">
+        <router-link to="/">
+          <img src="./assets/logo.png" />
+        </router-link>
+      </div>
+      <div id="side">
+        <router-link to="/transactions">Transactions</router-link>
+        <router-link to="/budget">Budget</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+
+body {
+  font-family: 'Lato', sans-serif;
+  font-weight: 400;
+  background-color: #f8f9fa;
+  color: #484e61;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  display: flex;
+  align-items: center;
+  padding: 10px 40px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#side {
+  padding-right: 30px;
+  padding-bottom: 10px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#side a {
+  text-decoration: none;
+  font-size: 18px;
+  color: #484e61;
+  padding: 0px 20px;
+}
+
+#side a:hover {
+  color: #e02800;
+}
+
+#brand {
+  margin-right: auto;
 }
 </style>
